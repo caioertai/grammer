d6374b8c77092edbfc0d831d25ed395ee4701948
Caio Andrade <caioertai@gmail.com>
2018-12-28 01:14:52 -0200

    Added grammed_by spec

--- spec/grammer/grammer_spec.rb
+++ spec/grammer/grammer_spec.rb
@@ -2,6 +2,19 @@
 
 require 'spec_helper'
 
+class TestUser
+  attr_reader :ig
+  grammed_by :ig
+  def initialize(username)
+    @ig = username
+  end
+end
+
 describe Grammer do
   it { expect { Grammer }.not_to raise_error }
+
+  describe 'grammed_by class macro' do
+    test_user = TestUser.new('caioertai')
+    it { expect(test_user).to be_a(Node) }
+  end
 end
