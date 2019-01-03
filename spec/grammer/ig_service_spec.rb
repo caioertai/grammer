16158eeced92f448f9010c1f3ec5540f41feb3d5
Caio Andrade <caioertai@gmail.com>
2019-01-03 13:27:28 -0200

    Better semantics on tests

--- spec/grammer/ig_service_spec.rb
+++ spec/grammer/ig_service_spec.rb
@@ -9,7 +9,7 @@ describe Grammer::IgService do
     it 'returns empty hash when no instagram user was found' do
       not_found_page = File.read('spec/fixtures/instagram_user_not_found.html')
       allow(described_class).to receive_message_chain(:get) { not_found_page }
-      expect(subject.node('salkdjaslkjf')).to eq({})
+      expect(subject.node('invalid_instagram_user')).to eq({})
     end
   end
 end
