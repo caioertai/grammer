c47241ca44fbfea4d4b0a50e4e63b696134f5585
Caio Andrade <caioertai@gmail.com>
2019-01-04 15:22:13 -0200

    Returning array of media objects on Node#media

--- lib/grammer/media.rb
+++ lib/grammer/media.rb
@@ -2,8 +2,14 @@
 
 module Grammer
   class Media
+    def initialize(media_data)
+      
+    end
+
     def self.all_from(node)
-      node
+      node.data.dig('edge_owner_to_timeline_media', 'edges').map do |media|
+        new(media)
+      end
     end
   end
 end
