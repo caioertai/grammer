9d5809175bd2e3b8b76a72a8bf990b04fe15f047
Caio Andrade <caioertai@gmail.com>
2019-01-04 13:09:32 -0200

    Reduced complexity of macro

--- lib/grammer/macros.rb
+++ lib/grammer/macros.rb
@@ -9,25 +9,31 @@ module Grammer
   # Defines class macros
   module Macros
     def get_or_set_const(module_name)
-      const_set(module_name, Module.new) unless const_defined?(module_name)
-      const_get(module_name)
+      if const_defined?(module_name)
+        const_get(module_name)
+      else
+        const_set(module_name, Module.new)
+      end
     end
 
-    def define_module_method(mod, method, options)
+    def define_module_method(mod, method_name, opts)
       mod.class_eval do
-        target_attribute = options[:on]
+        target_attribute = opts[:on]
         define_method(target_attribute) do
-          instance_var = "@#{target_attribute}"
-          node = Node.new(send(method), service: options[:service])
-          instance_variable_set(instance_var, node) unless instance_variable_defined?(instance_var)
-          instance_variable_get(instance_var)
+          instance_variable_name = "@#{target_attribute}"
+          if instance_variable_defined?(instance_variable_name)
+            instance_variable_get(instance_variable_name)
+          else
+            node = Node.new(send(method_name), service: opts[:service])
+            instance_variable_set(instance_variable_name, node)
+          end
         end
       end
     end
 
-    def grammed_by(method, options = {})
+    def grammed_by(method_name, opts = {})
       mod = get_or_set_const(:"GammerMod#{object_id}")
-      define_module_method(mod, method, options)
+      define_module_method(mod, method_name, opts)
       prepend mod
     end
 
