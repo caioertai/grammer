7b6a734f7d80e98ae8077af76169315d89663d98
Caio Andrade <caioertai@gmail.com>
2019-01-04 10:52:45 -0200

    Module method defining

--- lib/grammer/macros.rb
+++ lib/grammer/macros.rb
@@ -13,23 +13,21 @@ module Grammer
       const_get(module_name)
     end
 
-    def grammed_by(method, options = {})
-      mod = get_or_set_const(:"GammerMod#{object_id}")
+    def define_module_method(mod, method, options)
       mod.class_eval do
         target_attribute = options[:on]
         define_method(target_attribute) do
-          username = send(method)
           instance_var = "@#{target_attribute}"
-
-          unless instance_variable_defined?(instance_var)
-            instance_variable_set(
-              instance_var,
-              Node.new(username, service: options[:service])
-            )
-          end
+          node = Node.new(send(method), service: options[:service])
+          instance_variable_set(instance_var, node) unless instance_variable_defined?(instance_var)
           instance_variable_get(instance_var)
         end
       end
+    end
+
+    def grammed_by(method, options = {})
+      mod = get_or_set_const(:"GammerMod#{object_id}")
+      define_module_method(mod, method, options)
       prepend mod
     end
 
