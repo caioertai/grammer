fa5b694827e47f403aa45d1666f4b6c6cb457d70
Caio Andrade <caioertai@gmail.com>
2019-04-28 18:55:14 -0300

    Using DataForwardable interface on Media

--- lib/grammer/media.rb
+++ lib/grammer/media.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 module Grammer
+  ##
+  # = Media
+  # Represents Instagram media
   class Media
+    extend DataForwardable
+    attr_data_forwarder(
+      image_url: 'display_url',
+      likes_count: %w[edge_liked_by count],
+      video?: 'is_video'
+    )
+
+    ##
+    # General Instagram hash of data from the media
     attr_reader :data
+
     def initialize(media_data)
       @data = media_data
-    end
-
-    def image_url
-      data.dig('display_url')
-    end
-
-    def likes_count
-      data.dig('edge_liked_by', 'count')
-    end
-
-    def video?
-      data.dig('is_video')
     end
 
     def self.all_from(node)
