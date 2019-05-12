d0e0c584acb914244c69ef3305fa28481e3b34b9
Caio Andrade <caioertai@gmail.com>
2019-05-11 21:07:08 -0300

    Dependency injection on IgService parsers

--- lib/grammer/ig_service.rb
+++ lib/grammer/ig_service.rb
@@ -6,16 +6,20 @@ module Grammer
   class IgService
     include HTTParty
     base_uri 'https://www.instagram.com'
+    def initialize(options = {})
+      @json_parser = options[:json_parser] || JSON
+      @html_parser = options[:html_parser] || Nokogiri
+    end
 
     ##
     # Requests Instagram node information with a given username.
     def node(username)
       page = self.class.get("/#{username}")
-      json = Nokogiri.parse(page)
-                     .at('body script')
-                     .text.match(/({.*})/)[1]
-      JSON.parse(json)
-          .dig('entry_data', 'ProfilePage', 0, 'graphql', 'user') || {}
+      json = @html_parser.parse(page)
+                         .at('body script')
+                         .text.match(/({.*})/)[1]
+      @json_parser.parse(json)
+                  .dig('entry_data', 'ProfilePage', 0, 'graphql', 'user') || {}
     end
   end
 end
--- spec/grammer/ig_service_spec.rb
+++ spec/grammer/ig_service_spec.rb
@@ -5,6 +5,22 @@ require 'spec_helper'
 describe Grammer::IgService do
   it { expect(described_class).to include(HTTParty) }
 
+  context "#initialize" do
+    it 'sets given JSON parser' do
+      JSONParser = Struct.new('JSONParser')
+      service = described_class.new(json_parser: JSONParser)
+      json_parser = service.instance_eval { @json_parser }
+      expect(json_parser).to eq(JSONParser)
+    end
+
+    it 'sets given HTML parser' do
+      HTMLParser = Struct.new('HTMLParser')
+      service = described_class.new(html_parser: HTMLParser)
+      html_parser = service.instance_eval { @html_parser }
+      expect(html_parser).to be(HTMLParser)
+    end
+  end
+
   context '#node' do
     it 'returns empty hash when no instagram user was found' do
       not_found_page = File.read('spec/fixtures/instagram_user_not_found.html')
